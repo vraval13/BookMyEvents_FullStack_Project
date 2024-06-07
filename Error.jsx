022e85b03bc9065e6af1572d85d5903fcc436931
Vyom Raval <125266587+vraval13@users.noreply.github.com>
2024-06-08 00:32:01 +0530

    first commit

--- Error.jsx
+++ Error.jsx
@@ -6,7 +6,7 @@ function Error()
       <section id="error-page">
         <div className="content"> 
           <h2 className="header">404</h2>
-          <h4>Sorry! Page not found</h4>
+          <h4>Sorry! Page not found , Please try Again !</h4>
           <p>
             Oops! It seems like the page you're trying to access doesn't exist.
             If you believe there's an issue , feel free to report it,and we'll look into it.
